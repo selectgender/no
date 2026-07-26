8846e522aa561677300f9a306d04849d3a381eec
selectgender <119613700+selectgender@users.noreply.github.com>
2026-07-26 07:51:08 -0700

    status line looking pretty legit

--- no.c
+++ no.c
@@ -1,48 +1,64 @@
 /*
-	# no
 	*no*thing fancy. my cute text editor!
 
-	# build:
-	```sh
 	cc -std=c89 -Wall -Wextra -Wpedantic -o no no.c
-	```
 
-	# goals
-	- [ ] modal
-	- [ ] search
-	- [ ] undo / redo
-	- [ ] range selection
-	- [ ] per filetype config
-
-	# implementation
-	if i havent written this by the time im finished with the program you
-	can remind me!!!
-
-	# why the name?
-	my favorite community college professor taught calculus III really well
-	and something that he would always say after an explanation is
-	"nothing fancy." math doesnt need to be hard. text editors dont need to
-	be hard either!
+	- search
+	- undo / redo
+	- range selection
+	- per filetype config
 */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-#define HEIGHT 24
-#define WIDTH 80
 #define GAP 256
 #define LINES_GAP 256
 #define VIEW_INIT 512
+#define SEARCH_SIZE 64
+#define STATUS_MAX 128
 #define TAB 8
+
+enum keys_extra {
+	ENTER = 13,
+	ESC = 27,
+	BACKSPACE = 127,
+
+	/*
+		not reported by the terminal directly, so we just assign this to
+		a random number
+	*/
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL,
+	HOME,
+	END,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
+enum status {
+	NONE,
+	SEARCH,
+	WRITTEN,
+	QUIT_CONFIRM
+};
+
+char *filename = NULL;
 
 char *buf = NULL;
 int buf_gap = 0;
-int buf_start = 0;
+int buf_pos = 0;
 int buf_size = 0;
 
 struct termios cooked = { 0 };
@@ -56,11 +72,24 @@ int view_line = 0;
 
 int *lines = NULL;
 int lines_gap = 0;
-int lines_start = 0;
+int lines_pos = 0;
 int lines_size = 0;
 int line = 0;
 int col = 0;
 int abs_col = 0;
+
+struct winsize ws = { 0 };
+
+enum status status = NONE;
+
+int searching = 0;
+char search[SEARCH_SIZE] = { 0 };
+int search_pos = 0;
+int search_gap = SEARCH_SIZE;
+int search_cursor = 0;
+
+int written = 0;
+int quit_confirm = 0;
 
 void buf_init(int argc, char **argv) {
 	FILE *f = NULL;
@@ -68,21 +97,22 @@ void buf_init(int argc, char **argv) {
 
 	if (argc != 2) {
 		printf("file input please :3");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	f = fopen(argv[1], "r");
+	filename = argv[1];
+	f = fopen(filename, "r");
 
 	if (f == NULL) {
 		printf("invalid file :(");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	fseek(f, 0, SEEK_END);
 	fsize = ftell(f);
 	rewind(f);
 
-	buf_start = 0;
+	buf_pos = 0;
 	buf_gap = GAP;
 	buf_size = fsize + buf_gap;
 	buf = malloc(sizeof(char) * buf_size);
@@ -91,7 +121,7 @@ void buf_init(int argc, char **argv) {
 }
 
 char buf_get_char(int i) {
-	if (i < buf_start) {
+	if (i < buf_pos) {
 		return buf[i];
 	} else {
 		return buf[i + buf_gap];
@@ -99,59 +129,69 @@ char buf_get_char(int i) {
 }
 
 void buf_move_left(int n) {
-	if (n > buf_start) {
-		n = buf_start;
+	int src = 0;
+	int dst = 0;
+
+	if (n > buf_pos) {
+		n = buf_pos;
 	}	
-	
-	memcpy(buf + buf_start + buf_gap - n, buf + buf_start - n, n);
-	buf_start -= n;
+
+	src = buf_pos - n;
+	dst = src + buf_gap;
+	memcpy(&buf[dst], &buf[src], n * sizeof(char));
+	buf_pos -= n;
 }
 
 void buf_move_right(int n) {
-	if (n > buf_size - buf_gap - buf_start) {
-		n = buf_size - buf_gap - buf_start;
+	int src = 0;
+	int dst = 0;
+
+	if (n > buf_size - buf_gap - buf_pos) {
+		n = buf_size - buf_gap - buf_pos;
 	}
 
-	memcpy(buf + buf_start, buf + buf_start + buf_gap, n);
-	buf_start += n;
+	src = buf_pos + buf_gap;
+	dst = buf_pos;
+	memcpy(&buf[dst], &buf[src], n * sizeof(char));
+	buf_pos += n;
 }
 
-void buf_move_to(int n) {
-	if (n < buf_start) {
-		buf_move_left(buf_start - n);
-	} else if (n > buf_start) {
-		buf_move_right(n - buf_start);
+void buf_move(int n) {
+	if (n < buf_pos) {
+		buf_move_left(buf_pos - n);
+	} else if (n > buf_pos) {
+		buf_move_right(n - buf_pos);
 	}
 }
 
 void buf_grow(int n) {
-	int bump;
-	char *dst;
-	char *src;
-	size_t size;
+	int bump = 0;
+	int dst = 0;
+	int src = 0;
+	int size = 0;
 
 	if (buf_gap >= n) {
 		return;
 	}
 
-	bump = ((n / GAP) + 1) * GAP;
+	bump = (((n - buf_gap) / GAP) + 1) * GAP;
 	buf = realloc(buf, sizeof(char) * (buf_size + bump));
-	src = buf + buf_start + buf_gap;
+	src = buf_pos + buf_gap;
 	dst = src + bump;
-	size = buf_size - buf_start - buf_gap + 1;
-	memcpy(dst, src, size);
+	size = buf_size - buf_pos - buf_gap;
+	memcpy(&buf[dst], &buf[src], size);
 	buf_gap += bump;
 	buf_size += bump;
 }
 
 void buf_insert_char(char c) {
 	buf_grow(1);
-	buf[buf_start++] = c;
+	buf[buf_pos++] = c;
 	buf_gap--;
 }
 
-int lines_get_line(int i) {
-	if (i < lines_start) {
+int lines_get(int i) {
+	if (i < lines_pos) {
 		return lines[i];
 	} else {
 		return lines[i + lines_gap];
@@ -159,54 +199,64 @@ int lines_get_line(int i) {
 }
 
 void lines_move_left(int n) {
-	if (n > lines_start) {
-		n = lines_start;
+	int src = 0;
+	int dst = 0;
+
+	if (n > lines_pos) {
+		n = lines_pos;
 	}	
 	
-	memcpy(lines + lines_start + lines_gap - n, lines + lines_start - n, n);
-	lines_start -= n;
+	src = lines_pos - n;
+	dst = src + lines_gap;
+	memcpy(&lines[dst], &lines[src], n * sizeof(int));
+	lines_pos -= n;
 }
 
 void lines_move_right(int n) {
-	if (n > lines_size - lines_gap - lines_start) {
-		n = lines_size - lines_gap - lines_start;
+	int src = 0;
+	int dst = 0;
+
+	if (n > lines_size - lines_gap - lines_pos) {
+		n = lines_size - lines_gap - lines_pos;
 	}
 
-	memcpy(lines + lines_start, lines + lines_start + lines_gap, n);
-	lines_start += n;
+	src = lines_pos + lines_gap;
+	dst = lines_pos;
+	memcpy(&lines[dst], &lines[src], n * sizeof(int));
+	lines_pos += n;
 }
 
-void lines_move_to(int n) {
-	if (n < lines_start) {
-		lines_move_left(lines_start - n);
-	} else if (n > lines_start) {
-		lines_move_right(n - lines_start);
+void lines_move(int n) {
+	if (n < lines_pos) {
+		lines_move_left(lines_pos - n);
+	} else if (n > lines_pos) {
+		lines_move_right(n - lines_pos);
 	}
 }
 
 void lines_grow(int n) {
-	int bump;
-	int *dst;
-	int *src;
-	size_t size = 0;
+	int bump = 0;
+	int dst = 0;
+	int src = 0;
+	int size = 0;
 
 	if (lines_gap >= n) {
 		return;
 	}
 
-	bump = ((n / LINES_GAP) + 1) * LINES_GAP;
+	bump = (((n - lines_gap) / LINES_GAP) + 1) * LINES_GAP;
 	lines = realloc(lines, sizeof(int) * (lines_size + bump));
-	src = lines + lines_start + lines_gap;
+	src = lines_pos + lines_gap;
 	dst = src + bump;
-	size = lines_size - lines_start - lines_gap + 1;
-	memcpy(dst, src, size);
+	size = lines_size - lines_pos - lines_gap;
+	memcpy(&lines[dst], &lines[src], size);
 	lines_gap += bump;
 	lines_size += bump;
 }
 
 void lines_insert(int i) {
 	lines_grow(1);
-	lines[lines_start++] = i;
+	lines[lines_pos++] = i;
 	lines_gap--;
 }
 
@@ -231,11 +281,113 @@ void lines_init(void) {
 }
 
 int lines_get_len(int i) {
-	if (i != lines_size - lines_gap) {
-		return lines_get_line(i + 1) - lines_get_line(i);
+	if (i != lines_size - lines_gap - 1) {
+		return lines_get(i + 1) - lines_get(i);
 	} else {
-		return buf_size - buf_gap - lines_get_line(i);
+		return buf_size - buf_gap - lines_get(i);
 	}
+}
+
+int search_used(void) {
+	return SEARCH_SIZE - search_gap;
+}
+
+void search_prev(void) {
+	char *ptr = memmem(&buf[buf_pos + buf_gap], buf_size - buf_gap - buf_pos, search, search_used());
+
+	if (ptr == NULL) {
+		return;
+	}
+
+	
+}
+
+void search_next(void) {
+	/* memmem after the gap */
+}
+
+void search_cursor_left(void) {
+	if (search_cursor > 0) {
+		--search_cursor;
+	}
+}
+
+void search_cursor_right(void) {
+	if (search_cursor < search_used()) {
+		++search_cursor;		
+	}
+}
+
+void search_cursor_home(void) {
+	search_cursor = 0;
+}
+
+void search_cursor_end(void) {
+	search_cursor = search_used();
+}
+
+void search_move_left(int n) {
+	int src = 0;
+	int dst = 0;
+
+	if (n > search_pos) {
+		n = search_pos;
+	}	
+
+	src = search_pos - n;
+	dst = src + search_gap;
+	memcpy(&search[dst], &search[src], n * sizeof(char));
+	search_pos -= n;
+}
+
+void search_move_right(int n) {
+	int src = 0;
+	int dst = 0;
+
+	if (n > SEARCH_SIZE - search_gap - search_pos) {
+		n = SEARCH_SIZE - search_gap - search_pos;
+	}
+
+	src = search_pos + search_gap;
+	dst = search_pos;
+	memcpy(&search[dst], &search[src], n * sizeof(char));
+	search_pos += n;
+}
+
+void search_move(int n) {
+	if (n < search_pos) {
+		search_move_left(search_pos - n);
+	} else if (n > search_pos) {
+		search_move_right(n - search_pos);
+	}
+}
+
+void search_delete(void) {
+/*
+	if () {
+		search_move(search_cursor);
+		search_gap++;
+	}
+*/
+}
+
+void search_backspace(void) {
+/*
+	if () {
+		search_move(search_cursor);
+		search
+		search_gap++;
+	}
+*/
+}
+
+void search_insert(int c) {
+	if (search_gap == 0 || !isprint(c)) {
+		return;
+	}
+
+	search[search_pos++] = c;
+	--search_gap;
 }
 
 void view_init(void) {
@@ -264,7 +416,7 @@ void view_push_char(char c) {
 
 void view_write_char(char c) {
 	if (c == '\n') {
-		view_push("\r\n\x1b[K", 5);
+		view_push("\r\n", 2);
 		view_rows++;
 	} else if (c == '\t') {
 		int i;
@@ -272,18 +424,37 @@ void view_write_char(char c) {
 		for (i = 0; i < TAB; i++) {
 			view_push_char(' ');
 		}
+	} else if (!isprint(c)) {
+		view_push_char('?');
 	} else {
 		view_push_char(c);
 	}
 }
 
-void view_write_cursor(void) {
-	char cbuf[32];
-	int len;
+void view_write_status(void) {
+	switch (status) {
+	case NONE:
+		break;
+	case SEARCH:
+		view_push("SEARCH: ", 8);
+		view_push(search, search_used());
+		break;
+	case WRITTEN:
+		view_push("written", 7);
+		break;
+	case QUIT_CONFIRM:
+		view_push("unsaved changes, press again to quit!", 37);
+		break;
+	}
+}
+
+void view_write_cursor_normal(void) {
+	char cbuf[32] = { 0 };
+	int len = 0;
 	int x = col + 1;
 	int y = line - view_line;
-	int i;
-	int line_len = lines_get_line(line);
+	int i = 0;
+	int line_len = lines_get(line);
 
 	for (i = line_len; i < line_len + col; i++) {
 		if (buf_get_char(i) == '\t') {
@@ -295,25 +466,48 @@ void view_write_cursor(void) {
 	view_push(cbuf, len);
 }
 
+void view_write_cursor_search(void) {
+	char cbuf[32] = { 0 };
+	int len = 0;
+	int y = ws.ws_row;
+	int x = 8 + search_used() + 1;
+
+	len = snprintf(cbuf, sizeof(cbuf), "\x1b[%d;%dH", y, x);
+	view_push(cbuf, len);
+}
+
+void view_write_cursor(void) {
+	if (searching) {
+		view_write_cursor_search();
+	} else {
+		view_write_cursor_normal();
+	}
+}
+
 void view_write(void) {
 	int i;
 
 	view_used = 0;
 	view_rows = 0;
 
-	view_push("\x1b[H\x1b[K\x1b[?25l", 12);
+	view_push("\x1b[H\x1b[2J", 7);
 
-	for (i = lines_get_line(view_line); i < buf_start && view_rows < HEIGHT; i++) {
+	for (i = lines_get(view_line); i < buf_pos && view_rows < ws.ws_row - 1; i++) {
 		view_write_char(buf[i]);
 	}
 
-	for (i += buf_gap; i < buf_size && view_rows < HEIGHT; i++) {
+	for (i += buf_gap; i < buf_size && view_rows < ws.ws_row - 1; i++) {
 		view_write_char(buf[i]);
 	}
 
+	while (view_rows < ws.ws_row - 1) {
+		view_push("\r\n\x1b[K", 5);
+		view_rows++;
+	}
+
+	view_write_status();
 	view_write_cursor();
 
-	view_push("\x1b[?25h", 6);
 	write(STDOUT_FILENO, view, view_used);
 }
 
@@ -341,6 +535,15 @@ void alt_buf_off(void) {
 	write(STDOUT_FILENO, "\x1b[?1049l", 8);
 }
 
+void ws_update(void) {
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+}
+
+void ws_signal_update(int _) {
+	ws_update();
+	view_write();
+}
+
 void cursor_correct_col(void) {
 	int line_len = lines_get_len(line) - 1;
 
@@ -352,12 +555,12 @@ void cursor_correct_col(void) {
 }
 
 void cursor_down(void) {
-	if (line < lines_size - lines_gap) {
+	if (line < lines_size - lines_gap - 1) {
 		line++;
 		cursor_correct_col();
 	}
 
-	if (line - view_line > HEIGHT) {
+	if (line - view_line > ws.ws_row - 2) {
 		view_line++;
 	}
 }
@@ -387,7 +590,7 @@ void cursor_left(void) {
 	}
 }
 
-void cursor_beg(void) {
+void cursor_home(void) {
 	col = 0;
 	abs_col = col;
 }
@@ -397,10 +600,26 @@ void cursor_end(void) {
 	abs_col = col;
 }
 
+void cursor_page_up(void) {
+	int i;
+
+	for (i = 0; i < (ws.ws_row - 1) * 2 - 1; i++) {
+		cursor_up();
+	}
+}
+
+void cursor_page_down(void) {
+	int i;
+
+	for (i = 0; i < (ws.ws_row - 1) * 2 - 1; i++) {
+		cursor_down();
+	}
+}
+
 void decrement_lines(void) {
 	int i;
 
-	for (i = line + 1; i < lines_start; i++) {
+	for (i = line + 1; i < lines_pos; i++) {
 		lines[i]--;
 	}
 
@@ -410,33 +629,50 @@ void decrement_lines(void) {
 }
 
 void delete(void) {
-	int cursor = lines_get_line(line) + col;
-	
+	int cursor = lines_get(line) + col;
+
+	if (buf_pos + buf_gap >= buf_size) {
+		return;
+	}
+
+	written = 0;
+
 	if (buf_get_char(cursor) == '\n') {
-		lines_move_to(line + 1);
+		lines_move(line + 1);
 		lines_gap++;
 	}
 
-	buf_move_to(cursor);
+	buf_move(cursor);
 	buf_gap++;
 
 	decrement_lines();
 }
 
+void delete_to_end_of_line(void) {
+	int i = 0;
+	int n = lines_get_len(line) - col - 1;
+
+	for (i = 0; i < n; i++) {
+		delete();
+	}
+}
+
 void backspace(void) {
-	int cursor = lines_get_line(line) + col;
+	int cursor = lines_get(line) + col;
 
 	if (line == 0 && col == 0) {
 		return;
 	}
 
-	buf_move_to(cursor - 1);
+	written = 0;
+
+	buf_move(cursor - 1);
 	buf_gap++;
 
 	if (col == 0) {
 		col = lines_get_len(line - 1) - 1;
 		abs_col = col;
-		lines_move_to(line);
+		lines_move(line);
 		lines_gap++;
 		line--;
 	} else {
@@ -450,31 +686,45 @@ void backspace(void) {
 void save(void) {
 	FILE *f;
 
-	f = fopen("TEST", "w");
+	f = fopen(filename, "w");
 
-	fwrite(buf, buf_start, 1, f);
-	fwrite(buf + buf_start + buf_gap, buf_size - buf_start - buf_gap, 1, f);
+	fwrite(buf, buf_pos, 1, f);
+	fwrite(buf + buf_pos + buf_gap, buf_size - buf_pos - buf_gap, 1, f);
 
 	fclose(f);
+
+	written = 1;
+	quit_confirm = 0;
+	status = WRITTEN;
 }
 
 void quit(void) {
+	if (!written && !quit_confirm) {
+		quit_confirm = 1;
+		status = QUIT_CONFIRM;
+	} else {
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void exiting(void) {
 	alt_buf_off();
 	raw_mode_off();
 	free(lines);
 	free(view);
 	free(buf);
-	exit(0);
 }
 
 void insert_char(char c) {
 	int i;
 
-	buf_move_to(lines_get_line(line) + col);
+	written = 0;
+
+	buf_move(lines_get(line) + col);
 	buf_insert_char(c);
 	col++;
 
-	for (i = line + 1; i < lines_start; i++) {
+	for (i = line + 1; i < lines_pos; i++) {
 		lines[i]++;
 	}
 
@@ -484,45 +734,105 @@ void insert_char(char c) {
 }
 
 void insert_line(void) {
-	lines_move_to(line + 1);
 	insert_char('\n');
-	lines_insert(lines_get_line(line) + col);
-	line++;
-	col = 0;
+	lines_move(line + 1);
+	lines_insert(lines_get(line) + col);
+	cursor_down();
+	cursor_home();
 }
 
-void input(void) {
-	char c;
+int input_process(void) {
+	char c = '\0';
+	char seq[3] = { 0 };
 
-	while (read(STDOUT_FILENO, &c, 1) == 0);
+	while (read(STDIN_FILENO, &c, 1) == 0);
 
+	if (c != ESC) {
+		return c;
+	}
+
+	/* put this in a while loop because we dont know how many characters can be in an escape sequence */
+	while (1) {
+		/* just incase this simply is an ESC input */
+		if (read(STDIN_FILENO, &seq[0], 1) == 0) {
+			return ESC;
+		} else if (read(STDIN_FILENO, &seq[1], 1) == 0) {
+			return ESC;
+		}
+
+		if (seq[0] != '[') {
+			continue;
+		}
+
+		/* extended escape sequence! */
+		if (seq[1] >= '0' && seq[1] <= '9') {
+			if (read(STDIN_FILENO, &seq[2], 1) == 0) {
+				return ESC;
+			}
+
+			if (seq[2] != '~') {
+				continue;
+			}
+
+			switch (seq[1]) {
+			case '3': return DEL;
+			case '5': return PAGE_UP;
+			case '6': return PAGE_DOWN;
+			}
+		} else {
+			switch (seq[1]) {
+			case 'A': return ARROW_UP;
+			case 'B': return ARROW_DOWN;
+			case 'C': return ARROW_RIGHT;
+			case 'D': return ARROW_LEFT;
+			case 'H': return HOME;
+			case 'F': return END;
+			}
+		}
+	}
+}
+
+void input_normal(int c) {
 	switch (c) {
-	case CTRL_KEY('n'):
+	case ARROW_DOWN:
 		cursor_down();
 		break;
-	case CTRL_KEY('p'):
+	case ARROW_UP:
 		cursor_up();
 		break;
-	case CTRL_KEY('f'):
+	case ARROW_RIGHT:
 		cursor_right();
 		break;
-	case CTRL_KEY('b'):
+	case ARROW_LEFT:
 		cursor_left();
 		break;
-	case CTRL_KEY('a'):
-		cursor_beg();
+	case HOME:
+		cursor_home();
 		break;
-	case CTRL_KEY('e'):
+	case END:
 		cursor_end();
+		break;
+	case PAGE_UP:
+		cursor_page_up();
+		break;
+	case PAGE_DOWN:
+		cursor_page_down();
+		break;
+	case BACKSPACE:
+		backspace();
+		break;
+	case ENTER:
+		insert_line();
 		break;
 	case CTRL_KEY('d'):
 		delete();
 		break;
-	case 127: /* this is the backspace key. will find a better way to represent this later. */
-		backspace();
+	case CTRL_KEY('k'):
+		delete_to_end_of_line();
 		break;
-	case 13: /* this is the enter key. will find a better way to represent this later. */
-		insert_line();
+	case CTRL_KEY('f'):
+		searching = 1;
+		status = SEARCH;
 		break;
 	case CTRL_KEY('s'):
 		save();
@@ -530,9 +840,58 @@ void input(void) {
 	case CTRL_KEY('q'):
 		quit();
 		break;
+	case '\0':
+		break;
 	default:
 		insert_char(c);
 		break;
+	}
+}
+
+void input_search(int c) {
+	switch (c) {
+	case ESC:
+	case ENTER:
+		searching = 0;
+		status = NONE;
+		break;
+	case ARROW_UP:
+		search_prev();
+		break;
+	case ARROW_DOWN:
+		search_next();
+		break;
+	case ARROW_LEFT:
+		search_cursor_left();
+		break;
+	case ARROW_RIGHT:
+		search_cursor_right();
+		break;
+	case HOME:
+		search_cursor_home();
+		break;
+	case END:
+		search_cursor_end();
+		break;
+	case DEL:
+		search_delete();
+		break;
+	case BACKSPACE:
+		search_backspace();
+		break;
+	default:
+		search_insert(c);
+		break;
+	}
+}
+
+void input(void) {
+	int c = input_process();
+
+	if (searching) {
+		input_search(c);
+	} else {
+		input_normal(c);
 	}
 }
 
@@ -542,11 +901,15 @@ int main(int argc, char **argv) {
 	view_init();
 	raw_mode_on();
 	alt_buf_on();
+	ws_update();
+
+	atexit(exiting);
+	signal(SIGWINCH, ws_signal_update);
 
 	while (1) {
 		view_write();
 		input();
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
